89503bbce4a19219e642fa4b242fde6d75123799
TienAnh-Luu <81581431+TienAnh-Luu@users.noreply.github.com>
2023-03-04 21:24:59 +0700

    change something

--- pages/index.js
+++ pages/index.js
@@ -19,12 +19,15 @@ export default function Home() {
 
       const data = await response.json();
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
 
       setResult(data.result);
       setAnimalInput("");
-    } catch(error) {
+    } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
@@ -34,22 +37,22 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>Chatbot</title>
+        <link rel='icon' href='/dog.png' />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <img src='/dog.png' className={styles.icon} />
+        <h3>My Chatbot</h3>
         <form onSubmit={onSubmit}>
           <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
+            type='text'
+            name='animal'
+            placeholder='Ask me anything'
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type='submit' value='Submit' />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
